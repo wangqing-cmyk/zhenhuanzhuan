f46e9beb2651ea580446a31acdda5d4fc2c33d9e
wangqing-cmyk <2444084821@qq.com>
2026-09-04 16:23:09 +0800

    Add js/index.js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    slidesPerView: 4,
+    centeredSlides: true,
+    paginationClickable: true,
+    spaceBetween: 30,
+    autoplay: 2500,
+    
+});
